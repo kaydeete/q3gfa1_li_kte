6e84f2113dc426ff4eac9abb0191da2b195e1657
kaydeedurant <143239699+kaydeete@users.noreply.github.com>
2024-02-21 15:27:14 +0800

    Add files via upload

---
+++ p2.js
@@ -0,0 +1,6 @@
+var firstname = prompt("First Name: ");
+var lastname = prompt("Last Name: ");
+var birthyear = prompt("Birth Year: ");
+var age = 2024-birthyear;
+
+document.getElementById("output").innerHTML = "Hello " + firstname + " " + lastname + "! How does it feel to be " + age + " years old?";
